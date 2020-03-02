e740fe75a6d93f7f4de4337550946a16a0ff4300
THC-QA <Tristan.Clapp@academytrainee.com>
2020-03-02 17:53:25 +0000

    mySQL update.

--- hotsauce_database.sql
+++ hotsauce_database.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS ingredients;
 ​
 CREATE TABLE ingredients (
     id INT(5) NOT NULL AUTO_INCREMENT,
-    ingredient_name VARCHAR(10) NOT NULL,
+    ingredient_name VARCHAR(30) NOT NULL,
     ingredient_type VARCHAR(10) NOT NULL,
     PRIMARY KEY(id)
 );
@@ -14,14 +14,14 @@ DROP TABLE IF EXISTS recipes;
 CREATE TABLE recipes (
     id INT(5) NOT NULL AUTO_INCREMENT,
     recipe_name VARCHAR(50) NOT NULL,
-    ingredient_type VARCHAR(10000) NOT NULL,
+    recipe_method VARCHAR(10000) NOT NULL,
     PRIMARY KEY(id)
 );
 
-DROP TABLE IF EXISTS rec_ing;
+DROP TABLE IF EXISTS recipe_ingredients;
 ​
-CREATE TABLE ingredients (
-    rec_ing_id INT(5) NOT NULL AUTO_INCREMENT,
+CREATE TABLE recipe_ingredients (
+    id INT(5) NOT NULL AUTO_INCREMENT,
     recipe_id INT(5) NOT NULL,
     ingredient_id INT(5) NOT NULL,
     PRIMARY KEY(id),
